1d17a49ffee1d404bb386a6f14f7b66946114774
eddie wang <wang8119@gmail.com>
2024-03-13 08:52:11 +0800

    Session

---
+++ javascript/網路/Session/上課.js
@@ -0,0 +1,15 @@
+// https://cscie12.dce.harvard.edu/lecture_notes/2018-fall/20181204/slide2.html
+
+// 為什麼要有secction
+// 都有expire 設定
+// 4kb
+
+// http 無狀態協定 登入狀態 會忘記
+// section id
+// 記錄某個資料狀態
+
+// -後端溝通
+// -伺服器
+
+// -stuts
+// -不同
