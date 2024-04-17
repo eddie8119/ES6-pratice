1bf82070427979449aae9f869eed8558d8d4a4f2
eddie wang <wang8119@gmail.com>
2024-04-17 21:36:32 +0800

    更新

--- javascript/網路/dns.js
+++
@@ -1,8 +0,0 @@
-// DNS工作原理 | 什么是DNS 它如何工作 https://www.youtube.com/watch?v=ZqWb5nNzTf8
-
-// 域名去址向ip位置(你以前訪問過就會存在緩存) 透過dns server幫你解析對應
-// dns server發送查詢請求
-
-// 對應關係
-// 域名分層: 根域根域名 - 頂級域名 - 一級域名;
-// DNS分層協作機制: root dns server - top level dns server - name dns server
